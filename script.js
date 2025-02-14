d05409174928d928637bb159389a313acf60c2af
Amit <81560269+immeas@users.noreply.github.com>
2025-02-14 12:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-    // Get references to the select dropdown and button
-    let colorSelect = document.getElementById("colorSelect");
-    let removeButton = document.querySelector("input[type='button']");
+window.onload = function () {
+    document.querySelector("input[type='button']").onclick = function () {
+        let colorSelect = document.getElementById("colorSelect");
+        if (colorSelect.value) colorSelect.remove(colorSelect.selectedIndex);
+    };
+};
 
-    // Add event listener to the button
-    removeButton.addEventListener("click", function () {
-        let selectedIndex = colorSelect.selectedIndex;
-        
-        // Ensure an option is selected before attempting to remove it
-        if (selectedIndex !== -1) {
-            colorSelect.remove(selectedIndex);
-        }
-    });
-});
